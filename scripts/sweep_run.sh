2308c9688db3af6b76b4d5602de2b666acebcf56
Xavier Gonzalez <xavier18@stanford.edu>
2024-12-02 09:59:06 -0800

    update sweep_run to run a wandb sweep

--- scripts/sweep_run.sh
+++ scripts/sweep_run.sh
@@ -1,23 +1,29 @@
-#!/usr/bin/bash
-#SBATCH --job-name=bci_sweep
-#SBATCH --error=bci_sweep_%j_%a.err
-#SBATCH --out=bci_sweep_%j_%a.out
-#SBATCH --time=6-23:59:59
+#!/bin/bash
+#SBATCH --job-name=wandb_sweep          
+#SBATCH --output=wandb_sweep.%j.out     
+#SBATCH --time=23:59:59                 
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=4
-#SBATCH --constraint='GPU_SKU:A100_SXM4&GPU_MEM:80GB'
 #SBATCH --mail-type=ALL
 
+# Environment setup
 export DEEP_SSM_DATA=/scratch/groups/swl1
 module load cuda/12.4
 
-
+# Activate conda environment
 source /scratch/users/xavier18/miniconda3/bin/activate deep_ssm
 
-# Debugging
-env | grep PATH
-which ninja
-python -m pip show ninja
+# Get Sweep ID from the command line argument
+SWEEP_ID=$1
 
+# Check if a Sweep ID was provided
+if [ -z "$SWEEP_ID" ]; then
+  echo "Error: No Sweep ID provided. Usage: sbatch wandb_agent.sbatch <SWEEP_ID>"
+  exit 1
+fi
 
-/scratch/users/xavier18/miniconda3/envs/deep_ssm/bin/python3 run.py -m $@
+# Construct the full Sweep Identifier
+FULL_SWEEP_ID="xavier_gonzalez/speech_bci/$SWEEP_ID"
+
+# Run the W&B agent
+wandb agent $FULL_SWEEP_ID
